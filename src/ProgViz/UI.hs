7722f5b92cf1dbde334d211471b6bbc4ecea6f1b
Tikhon Jelvis <tikhon@jelv.is>
2013-09-15 14:47:24 -0400

    Got the graph rendering working!

--- src/ProgViz/UI.hs
+++ src/ProgViz/UI.hs
@@ -7,22 +7,27 @@ import qualified Data.Map                    as M
 import qualified Graphics.UI.Threepenny      as UI
 import           Graphics.UI.Threepenny.Core
 
+import           Text.Printf
+
 import           ProgViz.Run
 import           ProgViz.State               (State)
 import           ProgViz.Types
 
 main :: IO ()
-main = startGUI defaultConfig { tpStatic = Just "/home/tikhon/Documents/programming/contests/a16zhack/src/" } setup
+main = startGUI defaultConfig {
+   tpStatic = Just "src/",
+   tpCustomHTML = Just "html/index.html"
+  } setup
 
 prelude, body :: String
-prelude = "x = 0\nls=[1,2,3]\nls2 = []"
+prelude = "g = Graph.Create('foo', [1,2,1,3,1,4,1,5,5,3,3,6,6,7,7,8,8,5])\nx = 0\nls=[1,2,3]\nls2 = []"
 body = "for i in ls:\n  i\n  x = 10 + x + i\n  ls2.add(i * 10)"
 
 setup :: Window -> IO ()
 setup window = void $ do
   _ <- return window # set title "ProgViz"
   UI.addStyleSheet window "main.css"
-  getBody window #+ renderState (head . snd $ test prelude body)
+  getBody window #+ (renderState (head . snd $ test prelude body) ++ [mkElement "script" # set (attr "id") "js/raphael.js"])
 
 list :: Show a => [a] -> IO Element
 list ls = foldl go (UI.ul #. "code-list") ls
@@ -37,11 +42,14 @@ render (Str s)           = UI.div #. "value" #+ [string s]
 render (Bool b)          = UI.div #. "value" #+ [string $ show b]
 render (List ls)         = list ls
 render (Node idn _ _)    = UI.div #. "node"  #+ [string $ show idn]
-render (Graph _ _ edges) = UI.div #. "graph" #+ [string . show $ fst <$> edges,
-                                                string . show $ snd <$> edges]
+render (Graph _ _ edges) = UI.div #. "graph" #+ [serialize "fst" $ fst <$> edges,
+                                                 serialize "snd" $ snd <$> edges]
+  where serialize which idns =
+          UI.span #. printf "graph-%s" which #+ [string $ show idns]
 
 renderLabeled :: (String, Value) -> IO Element
-renderLabeled (str, val) = label (str ++ ": ") $ render val
+renderLabeled (_, val@Graph {}) = render val
+renderLabeled (str, val)        = label (str ++ ": ") $ render val
 
 renderState :: State -> [IO Element]
 renderState = map renderLabeled . M.toList
